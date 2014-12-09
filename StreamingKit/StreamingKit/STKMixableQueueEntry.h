cd17db17611472b7d94bd07897759410aa1b6f9d
James Gordon <james.gordon@thisisglobal.com>
2014-12-09 15:25:36 +0000

    Initial add of mixable queue entry object

---
+++ StreamingKit/StreamingKit/STKMixableQueueEntry.h
@@ -0,0 +1,13 @@
+//
+//  STKMixableQueueEntry.h
+//  StreamingKit
+//
+//  Created by James Gordon on 09/12/2014.
+//  Copyright (c) 2014 Thong Nguyen. All rights reserved.
+//
+
+#import <StreamingKit/StreamingKit.h>
+
+@interface STKMixableQueueEntry : STKQueueEntry
+
+@end
---
+++ StreamingKit/StreamingKit/STKMixableQueueEntry.m
@@ -0,0 +1,13 @@
+//
+//  STKMixableQueueEntry.m
+//  StreamingKit
+//
+//  Created by James Gordon on 09/12/2014.
+//  Copyright (c) 2014 Thong Nguyen. All rights reserved.
+//
+
+#import "STKMixableQueueEntry.h"
+
+@implementation STKMixableQueueEntry
+
+@end
